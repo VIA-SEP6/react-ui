0125d679c9b535e923df536835781d35b2ca0c7c
David Franko <david.franko1998@gmail.com>
2021-05-12 23:19:40 +0200

    SEP6-26 - Added endpoints for adding movie to favorites

--- src/components/Movie/Details/FavoriteMovieIcon.js
+++ src/components/Movie/Details/FavoriteMovieIcon.js
@@ -2,7 +2,7 @@ import {makeStyles} from "@material-ui/core/styles";
 import {Icon, IconButton} from "@material-ui/core";
 import React, {useEffect, useState} from "react";
 import firestoreReferenceService from "../../../services/firebase/firestore/references";
-import movieApiService from "../../../services/firebase/api/movie";
+import authApiService from "../../../services/firebase/api/user";
 
 const useStyles = makeStyles(theme => ({
     root: {}
@@ -16,9 +16,9 @@ export default function FavoriteMovieIcon(props) {
 
     const handleClick = () => {
         if (isFavorite)
-            movieApiService.removeMovieFromFavourites(`${movie.id}`, currentUser.uid)
+            authApiService.removeMovieFromFavourites(`${movie.id}`, currentUser.uid)
         else
-            movieApiService.addMovieToFavourites(`${movie.id}`, currentUser.uid)
+            authApiService.addMovieToFavourites(`${movie.id}`, currentUser.uid)
     }
 
     const favoriteIcon = (
@@ -28,17 +28,18 @@ export default function FavoriteMovieIcon(props) {
     )
 
     useEffect(() => {
-        firestoreReferenceService
-            .getFavoriteMoviesByUserIdReference(currentUser.uid)
-            .onSnapshot(
-                docSnapshot => {
-                    const favouriteMovies = docSnapshot.data().favouriteMovies || [];
-                    setIsFavorite(favouriteMovies.includes(`${movie.id}`))
-                },
-                error => {
-                    console.log(`Encountered error: ${error}`);
-                })
-    }, [setIsFavorite])
+        if (currentUser)
+            firestoreReferenceService
+                .getFavoriteMoviesByUserIdReference(currentUser.uid)
+                .onSnapshot(
+                    docSnapshot => {
+                        const favouriteMovies = docSnapshot.data().favouriteMovies || [];
+                        setIsFavorite(favouriteMovies.includes(`${movie.id}`))
+                    },
+                    error => {
+                        console.log(`Encountered error: ${error}`);
+                    })
+    }, [setIsFavorite, currentUser])
 
     return (
         <div className={classes.root}>
